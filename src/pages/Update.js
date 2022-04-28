a6c7dce13730189e284165f30c6214e417456ba4
MuratErtugral <muratertugral93@gmail.com>
2022-04-28 15:03:43 +0300

    update 1.0

--- src/pages/Update.js
+++ src/pages/Update.js
@@ -1,13 +1,137 @@
-import React from 'react'
+import React, { useContext, useState } from 'react';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Block from "../assets/blok.png";
+import { BlogContext } from "../contexts/BlogContext"
 import { useLocation } from 'react-router-dom';
 
-const Update = () => {
+
+
+
+
+
+
+
+
+const theme = createTheme();
+
+
+export default function Update() {
+
   const location = useLocation();
   const item = location.state.item;
-  console.log(item)
-  return (
-    <div>Update</div>
-  )
-}
+  
+  const initialValues={...item};
+  const int = {...item}
+  console.log(int)
+  
+  const { AddNewBlog } = useContext(BlogContext);
+  const [blog, setBlog] = useState(initialValues)
+  
+  const handleChange = (e) => {
+    e.preventDefault();
+    const { name,value } = e.target
+    
+    setBlog({...blog, [name]:value})
+  }
 
-export default Update
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(blog);
+    AddNewBlog(blog)
+    setBlog(initialValues)
+    
+  };
+  
+
+  return (
+    
+    <div >
+    <ThemeProvider theme={theme}>
+      <Container sx={{backgroundColor :"white",}} component="main" maxWidth="xs"  >
+        <CssBaseline />
+        <Box
+          sx={{
+            
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            
+            
+          }}
+        >
+          
+            <img src={Block} alt="block" style={{margin:"1rem",padding:"1rem",borderRadius: "50%" , backgroundColor:"gray"}} />
+          
+          <Typography component="h1" variant="h5">
+          ──── UPDATE BLOG ────
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit}  sx={{ mt: 3  }}>
+            <Grid container spacing={2}>
+             
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="title"
+                  label="Title"
+                  name="title"
+                  value={blog.title}
+                  onChange={handleChange}
+                  
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="image"
+                  label="Image URL"
+                  type="url"
+                  id="url"
+                  value={blog.image}
+                  onChange={handleChange}
+                  
+                />
+              </Grid>
+              <Grid item xs={12} l={12}>
+              <TextField
+                id="outlined-multiline-static"
+                name="content"
+                required
+                label="Content"
+                multiline
+                // value={}
+                fullWidth
+                rows={10}
+                value={blog.content}
+                onChange={handleChange}
+               
+                />
+              </Grid>
+
+              
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Submit
+            </Button>
+ 
+          </Box>
+        </Box>
+        
+      </Container>
+    </ThemeProvider>
+    </div>
+  );
+}
